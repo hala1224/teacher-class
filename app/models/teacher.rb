174915c2f9ab1102ed708a835968f586f741f431
Natalie Halawani <hala1224@gmail.com>
2019-03-21 15:25:39 +0000

    modified teacher.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,16 @@
 class Teacher < ActiveRecord::Base
+ has_secure_password
+ 
   has_many :categories
+  
+  has_secure_password
+
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Teacher.all.find{|user| user.slug == slug}
+  end
 end
